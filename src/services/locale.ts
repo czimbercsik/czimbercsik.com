b8cab778fe975944fa81ed6376b3ca39813ed609
LilSylent <github@czimbercsik.com>
2025-12-07 22:04:35 +0100

    fix: getUserLocale function locale validation

--- src/services/locale.ts
+++ src/services/locale.ts
@@ -1,14 +1,21 @@
 'use server'
 
-import { Locale, defaultLocale } from '@/src/i18n/config';
+import { Locale, defaultLocale, locales } from '@/src/i18n/config';
 import { cookies, headers } from 'next/headers';
 
 const COOKIE_NAME = 'CZIMBERCSIK_LOCALE';
 
 export async function getUserLocale() {
-  return (await cookies()).get(COOKIE_NAME)?.value ||
-    (await headers()).get('accept-language')?.slice(0, 2) ||
-    defaultLocale;
+  const cookieLocale = (await cookies()).get(COOKIE_NAME)?.value;
+  const headerLocale = (await headers()).get('accept-language')?.slice(0, 2);
+
+  const detectedLocale = cookieLocale || headerLocale || defaultLocale;
+
+  if(!locales.includes(detectedLocale as Locale)) {
+    return defaultLocale;
+  }
+
+  return detectedLocale as Locale;
 }
 
 export async function setUserLocale(locale: Locale) {
